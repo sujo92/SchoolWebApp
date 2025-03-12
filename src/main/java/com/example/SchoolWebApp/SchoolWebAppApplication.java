05540939c8dcb3fb7a4b12fe15b8820b5197921d
Sujata Patil <sujatapatil2010@gmail.com>
2025-03-12 16:43:33 -0700

    add auditing support by spring data jpa

--- src/main/java/com/example/SchoolWebApp/SchoolWebAppApplication.java
+++ src/main/java/com/example/SchoolWebApp/SchoolWebAppApplication.java
@@ -3,11 +3,13 @@ package com.example.SchoolWebApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.example.SchoolWebApp.repository")
 @EntityScan("com.example.SchoolWebApp.model")
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class SchoolWebAppApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/SchoolWebApp/service/ContactService.java
+++ src/main/java/com/example/SchoolWebApp/service/ContactService.java
@@ -7,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,8 +25,6 @@ public class ContactService {
     public boolean saveMessageDetails(Contact contact){
         boolean isSaved = false;
         contact.setStatus(EazySchoolConstants.OPEN);
-        contact.setCreatedBy(EazySchoolConstants.ANONYMOUS);
-        contact.setCreatedAt(LocalDateTime.now());
         Contact savedContact = contactRepository.save(contact);
         if(null != savedContact && savedContact.getContactId()>0) {
             isSaved = true;
@@ -40,13 +37,11 @@ public class ContactService {
         return contactMsgs;
     }
 
-    public boolean updateMsgStatus(int contactId, String updatedBy){
+    public boolean updateMsgStatus(int contactId){
         boolean isUpdated = false;
         Optional<Contact> contact = contactRepository.findById(contactId);
         contact.ifPresent(contact1 -> {
             contact1.setStatus(EazySchoolConstants.CLOSE);
-            contact1.setUpdatedBy(updatedBy);
-            contact1.setUpdatedAt(LocalDateTime.now());
         });
         Contact updatedContact = contactRepository.save(contact.get());
         if(null != updatedContact && updatedContact.getUpdatedBy()!=null) {
