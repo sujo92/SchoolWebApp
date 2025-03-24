90def72aa210da1c5f1c2f2b6fbfe695a25b8781
Sujata Patil <sujatapatil2010@gmail.com>
2025-03-24 10:19:14 -0700

    added custom queries

--- src/main/java/com/example/SchoolWebApp/repository/ContactRepository.java
+++ src/main/java/com/example/SchoolWebApp/repository/ContactRepository.java
@@ -1,9 +1,12 @@
 package com.example.SchoolWebApp.repository;
 
 import com.example.SchoolWebApp.model.Contact;
+import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +14,19 @@ import java.util.List;
 @Repository
 public interface ContactRepository extends JpaRepository<Contact, Integer> {
     List<Contact> findByStatus(String status);
+
+//    @Query("SELECT c FROM Contact c WHERE c.status = :status")
+    @Query(value = "SELECT * FROM contact_msg WHERE status = :status", nativeQuery = true)
     Page<Contact> findByStatus(String status, Pageable pageable);
+
+    @Transactional
+    @Modifying
+    @Query(value="Update Contact c SET c.status = ?1 where id = ?2")
+    int updateStatusById(String status, int id);
+
+    Page<Contact> findOpenMsgs(String status, Pageable pageable);
+
+    @Transactional
+    @Modifying
+    int updateMsgStatus(String status, int id);
 }
--- src/main/java/com/example/SchoolWebApp/service/ContactService.java
+++ src/main/java/com/example/SchoolWebApp/service/ContactService.java
@@ -11,8 +11,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 @Slf4j
 public class ContactService {
@@ -38,21 +36,18 @@ public class ContactService {
     public Page<Contact> findMsgsWithOpenStatus(int pageNum, String sortField, String sortDir){
         int pageSize = 5;
         Pageable pageable = PageRequest.of(pageNum-1, pageSize, sortDir.equals("asc")? Sort.by(sortField).ascending():Sort.by(sortField).descending());
-        Page<Contact> msgPage = contactRepository.findByStatus(EazySchoolConstants.OPEN, pageable);
+//        Page<Contact> msgPage = contactRepository.findByStatus(EazySchoolConstants.OPEN, pageable);
+        Page<Contact> msgPage = contactRepository.findOpenMsgs(EazySchoolConstants.OPEN, pageable);
         return msgPage;
     }
 
     public boolean updateMsgStatus(int contactId){
         boolean isUpdated = false;
-        Optional<Contact> contact = contactRepository.findById(contactId);
-        contact.ifPresent(contact1 -> {
-            contact1.setStatus(EazySchoolConstants.CLOSE);
-        });
-        Contact updatedContact = contactRepository.save(contact.get());
-        if(null != updatedContact && updatedContact.getUpdatedBy()!=null) {
+//        int rows = contactRepository.updateStatusById(EazySchoolConstants.CLOSE, contactId);
+        int rows = contactRepository.updateMsgStatus(EazySchoolConstants.CLOSE, contactId);
+        if(rows > 0) {
             isUpdated = true;
         }
         return isUpdated;
     }
-
 }
